65b7667bff89ff5b88b5ad6b90fce0e3379c5f09
David Lu <50630029+david1992121@users.noreply.github.com>
2022-06-23 14:38:27 +0800

    add ideas and comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  
   get 'pages/info'
   root to: redirect('/ideas')
   resources :ideas
+  resources :comments
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
